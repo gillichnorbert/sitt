b684d0674f6a068cb48af7a1e19d55eb380c16a9
“gillichnorbert” <gillich.norbert@icloud.com>
2026-09-13 15:21:01 +0200

    mindenféle faszság

--- src/app/components/prices/prices.component.ts
+++ src/app/components/prices/prices.component.ts
@@ -11,28 +11,24 @@ import { RouterModule } from '@angular/router';
 })
 export class PricesComponent {
   
-  // A 4 fő szolgáltatás árazási adatai
+  // Pontosan 2 nagy kártya adatai
   pricingItems = [
     {
-      title: 'Sittszállítás és Lomtalanítás',
-      price: '990 Ft',
-      unit: '/ Zsáktól',
-      desc: 'Tiszta sitt 14 000 Ft-tól, vegyes hulladék 16 000 Ft-tól. Teljes lomtalanítás megegyezés szerint, ingyenes felméréssel.',
-      icon: 'bi-trash3-fill'
+      title: 'Zsákos sittszállítás és Lomtalanítás',
+      desc: 'Teljes lomtalanítás ingyenes helyszíni felmérés alapján, teljes körű rakodással és gyors kiszállással.',
+      icon: 'bi-trash3-fill',
+      subPrices: [
+        { name: 'Tiszta sitt: (tégla, beton, csempe, vakolat, stb.)', value: '14 000 Ft / m³' },
+        { name: 'Vegyes sitt: (lom, papír, műanyag, fa, gipsz, stb.)', value: '16 000 Ft / m³' }
+      ]
     },
     {
-      title: 'Áruszállítás / Teherfuvarozás',
-      price: '20 000 Ft',
-      unit: '/ óra-tól',
-      desc: 'Gyors és biztonságos árutovábbítás. Városon belüli és Pest megyei fuvarozás. Fix áras konstrukció is kérhető.',
-      icon: 'bi-truck'
-    },
-    {
-      title: 'Gépi Földmunka',
-      price: '15 000 Ft',
-      unit: '/ gépóra-tól',
-      desc: 'Tereprendezés, alapásás és anyagmozgatás professzionális gépparkkal. Az ár tartalmazza a gépkezelő díját is.',
-      icon: 'bi-cone-striped'
+      title: 'Tehertaxi és Áruszállítás',
+      desc: 'Gyors és megbízható tehertaxi és áruszállítás biztonságosan, teljes körű rakodással.',
+      icon: 'bi-truck',
+      subPrices: [
+        { name: 'Áruszállítás / Teherfuvarozás', value: '20 000 Ft / óra-tól' },
+      ]
     }
   ];
 
